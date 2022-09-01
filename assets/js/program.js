531658fd4bdcff3442ff74954b405d78d6b3afae
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2022-09-01 11:36:27 +0200

    program: js elementer

---
+++ assets/js/program.js
@@ -0,0 +1,9 @@
+//program 
+
+// target div in section
+let row3 = document.querySelector('#row1');
+let row4 = document.querySelector('#row2')
+
+//content inside div
+row3.innerHTML = '<a href="/alice.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/IOT/alice_jennings.jpg"></a><a href="/joe.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/IOT/joe_peterson.jpg"></a><a href="/louise.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/IOT/louise_nielsen.jpg"></a><a href="/mamouhd.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/IOT/mamouhd_hassan.jpg"></a><a href="/olav.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/IOT/olav_eriksen.jpg"></a> '
+row4.innerHTML = '<a href="/chris.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/Desktop/chris_smith.jpg"></a><a href="/dan.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/Desktop/Dan_Jantzen.jpg"></a><a href="/line.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/Desktop/line_dalsgaard.jpg"></a><a href="/lis.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/Desktop/Lis_nielsen.jpg"></a><a href="/sarah.html"><img src="/assets/Ubuntu-materiale/Ubuntu-materiale/speakers/Desktop/sarah_souidan.jpg"></a>'
